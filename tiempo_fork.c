d5d92654e8d9ffc0b8905046d2a115eebb5a5673
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2026-02-12 16:58:35 -0600

    ejercicio 2.2

---
+++ tiempo_fork.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+
+    clock_t inicio, fin;
+    double tiempo;
+
+    inicio = clock();
+
+    for(int i = 0; i < 3; i++) {
+        pid_t pid = fork();
+
+        if(pid == 0) {
+            for(int j = 0; j < 1000000; j++) {}
+            return 0;
+        }
+    }
+
+    for(int i = 0; i < 3; i++) {
+        wait(NULL);
+    }
+
+    fin = clock();
+
+    tiempo = (double)(fin - inicio) / CLOCKS_PER_SEC;
+
+    printf("Tiempo con fork: %f segundos\n", tiempo);
+
+    return 0;
+}
